8661505924e3f16255e736774f65db327c8419c2
liuyangnorway <37050584+liuyangnorway@users.noreply.github.com>
2018-10-05 10:11:29 +0200

    Add files via upload

--- change_file_name.py
+++ change_file_name.py
@@ -6,6 +6,8 @@ Created on Thu Jun 21 13:02:50 2018
 """
 
 import os
+import time
+
 path = r'D:\Downloads\Download_5KPlayer\Neural Networks and Deep Learning'
 os.chdir(path)
 
@@ -17,4 +19,31 @@ for file in os.listdir(path):
              os.rename(os.path.join(path,file),os.path.join(path,file_new_name))
              print( file,'ok') 
      
+
+
+def findfile(root, keywords):
+    for fileDir, dirs, files in os.walk(root):
+        for file in files:
+            if keywords in file:
+                full_path = os.path.join(fileDir, file)
+                #print(os.path.normpath(os.path.abspath(full_path)))
+                mtime = os.path.getmtime(full_path)
+                datetime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(mtime))
+                print('{0}  {1}'.format(os.path.normpath(os.path.abspath(full_path)),datetime))
+
+
+
+root="E:" + os.sep+ "datacamp" + os.sep+ "find_finn"+ os.sep+"old"
+keywords= "fileA"
+findfile(root, keywords)
+
+fileDir="E:" + os.sep+ "datacamp" + os.sep+ "find_finn"
+keywords= "url_dict_new"
+findfile(fileDir, keywords)
+
+files=os.listdir(fileDir)
+for file in files:
+    if keywords in file:
+        full_path = os.path.join(fileDir, file)
+        print(os.path.normpath(os.path.abspath(full_path)))
         
